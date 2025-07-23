f2da8e74c8e103446d2b42062831d27099e6a349
nagendra <n91k16@gmail.com>
2025-07-23 22:08:52 +0530

    updated credentials

--- provider.tf
+++ provider.tf
@@ -1,4 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "289da116-5358-4835-8897-9e1db2cde3d2"
+  client_id =  "26d9845b-1dff-4b40-9e28-144e9aaaebb7"
+  client_secret = "tPT8QI-SHe1ym1m76GptwiWWFdZ6lAzSiDAbCG"
+  tenant_id  =  "150461e6-cf91-48a0-be99-6acb910fa44f"
 }
