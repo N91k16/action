b2a20ccec41bcda2af22ed55feea33a59efa1b4c
nagendra <n91k16@gmail.com>
2025-07-23 22:33:08 +0530

    pagla round

--- provider.tf
+++ provider.tf
@@ -9,8 +9,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+ client_id       = "26d9845b-1dff-4b40-9e28-144e9aaaebb7"
+  client_secret   = "tPT8QI-SHe1ym1m76GptwiWWFdZ6lAzSiDAbCG"
   subscription_id = "289da116-5358-4835-8897-9e1db2cde3d2"
-  client_id =  "26d9845b-1dff-4b40-9e28-144e9aaaebb7"
-  client_secret = "tPT8QI-SHe1ym1m76GptwiWWFdZ6lAzSiDAbCG"
-  tenant_id  =  "150461e6-cf91-48a0-be99-6acb910fa44f"
+  tenant_id       = "150461e6-cf91-48a0-be99-6acb910fa44f"
 }
+
